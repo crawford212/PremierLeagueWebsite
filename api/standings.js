33d93806d88c2f9e21ab4acd7ca81256cbf5d223
Oliver <145261170+crawford212@users.noreply.github.com>
2026-03-04 23:02:40 +0000

    Update standings.js

--- api/standings.js
+++ api/standings.js
@@ -1,12 +1,12 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch(
-      "https://v3.football.api-sports.io/standings?league=39&season=2024",
-      {
-        headers: { "x-apisports-key": process.env.API_KEY }
-      }
-    );
+    const league = req.query.league || 39;
+    const season = req.query.season || 2024;
 
+    const response = await fetch(
+      `https://v3.football.api-sports.io/standings?league=${league}&season=${season}`,
+      { headers: { "x-apisports-key": process.env.API_KEY } }
+    );
     const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
